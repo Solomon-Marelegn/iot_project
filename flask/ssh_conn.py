ac4e82eed3041bb58516c8eace2304ceb5020ee0
Solomon-Marelegn <135412912+Solomon-Marelegn@users.noreply.github.com>
2023-12-11 12:39:08 +0100

    Update ssh_conn.py

--- flask/ssh_conn.py
+++ flask/ssh_conn.py
@@ -19,7 +19,6 @@ def find_pid_by_name(process_name, ssh_client):
     pid = stdout.read().decode().strip().split()
     return pid
 
-
 def check_mqtt_broker():
     pass
 
@@ -27,7 +26,7 @@ def check_database_status(ssh_client):
     command = 'ps aux | grep mysql'
     stdin, stdout, stderr = ssh_client.exec_command(command)
     process_list = stdout.read().decode()
-    if 'mysqld' in process_list:
+    if 'mariadbd' in process_list:
         return 'On'
     else:
         return 'Off'
@@ -45,8 +44,7 @@ def check_mqtt_broker_status(ssh_client):
     command = 'ps aux | grep mosquitto'
     stdin, stdout, stderr = ssh_client.exec_command(command)
     process_list = stdout.read().decode()
-    
-    if 'mosquitto+' in process_list:
+    if '/mosquitto/mosquitto.conf' in process_list:
         return 'On'
     else:
         return 'Off'
@@ -56,6 +54,7 @@ def start_mqtt_subscriber(ssh_client):
     try:
         cmd = 'python3 mqtt_sub.py'
         stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    
         print("MQTT subscriber script started.")
     finally:
         ssh_client.close()
@@ -66,16 +65,14 @@ def stop_mqtt_subscriber(ssh_client):
         ssh_client.exec_command(f"kill {i}")
 
 def execute_command_on_vm(cmd):
-    host_list = {'Mqtt_Sub' : '10.120.0.49',
-                 'start' : '10.120.0.49',
-                 'stop' : '10.120.0.49',
+    host_list = {'Mqtt_Sub' : '10.120.0.60',
+                 'start' : '10.120.0.60',
+                 'stop' : '10.120.0.60',
                  'Mqtt_Broker': '10.120.0.23',
                  'Database': '10.120.0.76'
                 }
     client = create_ssh_client(host_list[cmd])
-    
     if cmd == 'start':
-        client = create_ssh_client()
         start_mqtt_subscriber(client)
     elif cmd == 'stop':
         stop_mqtt_subscriber(client)
@@ -85,6 +82,7 @@ def execute_command_on_vm(cmd):
         return check_mqtt_sub_status(client)
     elif cmd == 'Mqtt_Broker':
         return check_mqtt_broker_status(client)
+    
     
 
 
