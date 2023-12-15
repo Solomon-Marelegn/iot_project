4a16e50f312b64dc7228eb71d187f09d249eac77
Solomon-Marelegn <135412912+Solomon-Marelegn@users.noreply.github.com>
2023-12-15 09:19:28 +0100

    Update ping.py

--- pico/ping.py
+++ pico/ping.py
@@ -2,13 +2,14 @@ import socket
 import net
 
 def ping():
-    google_ip = "8.8.8.8"
-    port = 53 
+    
+    default_gateway = "192.168.2.1"
+    port = 53
 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
-        sock.connect((google_ip, port))
+        sock.connect((default_gateway, port))
         return True
 
     except Exception as e:
@@ -16,6 +17,4 @@ def ping():
 
     finally:
         sock.close()
-
-
 
